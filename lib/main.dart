de944ca0bd439d9be684d5c2a808419d7df3031c
Juna Im <dlawnsdk04@naver.com>
2025-05-13 22:54:50 +0900

    feat: FCM 기초 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_quill/flutter_quill.dart';
 import 'package:packup/Common/util.dart';
@@ -61,21 +64,76 @@ void main() async {
     javaScriptAppKey: JAVASCRIPT_APP_KEY,
   );
 
-  // socket
+  // ▼ socket
   await SocketService().initConnect();
+  // ▲ socket
 
-  // firebase
+  // ▼ firebase
   // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  final firebaseService = FirebaseService();
   await Firebase.initializeApp(
     // options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  final fireBaseInstance = await FirebaseMessaging.instance;
+  NotificationSettings settings = await fireBaseInstance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  /**
+   * authorized > 수신 허용
+   * denied > 수신 거부
+   */
+  print('수신 허용 여부: ${settings.authorizationStatus}');
+
   final fcmTokenKey = dotenv.env['FCM_TOKEN_KEY']!;
-  final fcmToken = await FirebaseMessaging.instance.getToken();
+  final fcmToken = await fireBaseInstance.getToken();
+  print("FCM 토큰 >> " + fcmToken.toString());
   saveToken(fcmTokenKey, fcmToken!);
 
-  FirebaseMessaging.onBackgroundMessage(FirebaseService.firebaseMessagingBackgroundHandler);
+  //FCM 토큰은 사용자가 앱을 삭제, 재설치 및 데이터 제거 등 토큰 유실 되었을 때 재발급 로직..?
+  fireBaseInstance.onTokenRefresh.listen((nToken) {
+
+  });
+
+  final localNotification = FlutterLocalNotificationsPlugin();
+  AndroidNotificationChannel? androidNotificationChannel;
+
+  if (Platform.isIOS) {
+    //await reqIOSPermission(fbMsg);
+  } else if (Platform.isAndroid) {
+    //Android 8 (API 26) 이상부터는 채널설정이 필수? 라고 한다...
+    androidNotificationChannel = const AndroidNotificationChannel(
+      'important_channel', // id
+      'Important_Notifications', // name
+      description: '팩업 중요 알림',
+      // description
+      importance: Importance.high,
+    );
+
+    await localNotification
+        .resolvePlatformSpecificImplementation<
+        AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(androidNotificationChannel);
+  }
+
+  // // 백그라운드 핸들러
+  FirebaseMessaging.onBackgroundMessage(FirebaseService.fcmBackgroundHandler);
+  //
+  // // 포그라운드 핸들러
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('[FCM - Foreground] MESSAGE.notification.title : ${message.notification?.title}');
+    print('[FCM - Foreground] MESSAGE.notification.body : ${message.notification?.body}');
+    firebaseService.fcmForegroundHandler(
+        message, localNotification, androidNotificationChannel);
+  });
+  // ▲ firebase
 
   runApp(
     MultiProvider(
--- lib/service/common/firebase_service.dart
+++ lib/service/common/firebase_service.dart
@@ -1,6 +1,7 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FirebaseService {
 
@@ -8,7 +9,41 @@ class FirebaseService {
   factory FirebaseService() => _instance;
   FirebaseService._internal();
 
-  static Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  //Dart VM은 네이티브 코드에서 Dart 코드로 접근할 수 있는 엔트리 포인트를 @pragma('vm:entry-point') 애너테이션을 통해 설정
+  @pragma('vm:entry-point')
+  static Future<void> fcmBackgroundHandler(RemoteMessage message) async {
     await Firebase.initializeApp();
+
+    if (message.notification != null) {
+      print('Background message received: ${message.notification}');
+    }
+  }
+
+  Future<void> fcmForegroundHandler(
+      RemoteMessage message,
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
+      AndroidNotificationChannel? channel) async {
+    print('[FCM - Foreground] MESSAGE : ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+      flutterLocalNotificationsPlugin.show(
+          message.hashCode,
+          message.notification?.title,
+          message.notification?.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel!.id,
+              channel.name,
+              channelDescription: channel.description,
+              icon: '@mipmap/ic_launcher',
+            ),
+            // iOS: const DarwinNotificationDetails(
+            //   badgeNumber: 1,
+            //   subtitle: 'the subtitle',
+            //   sound: 'slow_spring_board.aiff',
+            // ),
+          ));
+    }
   }
 }
