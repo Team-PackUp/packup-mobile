0a8004d1321288bb0d54c86231c408631d0c5ab8
jmj <waysoleward01@naver.com>
2025-06-07 22:20:54 +0900

    feat: 라우터 리다이렉트 로직 설정

--- lib/common/router.dart
+++ lib/common/router.dart
@@ -10,6 +10,7 @@ import 'package:packup/view/notice/notice_list.dart';
 import 'package:packup/view/notice/notice_view.dart';
 import 'package:packup/view/payment/toss/toss_result_screen.dart';
 import 'package:packup/view/user/preference/preference.dart';
+import 'package:packup/view/user/register_detail/register_detail.dart';
 import 'package:path/path.dart';
 
 GoRouter createRouter(UserProvider userProvider) {
@@ -20,26 +21,40 @@ GoRouter createRouter(UserProvider userProvider) {
     redirect: (context, state) {
       final isInitialized = userProvider.isInitialized;
       final accessToken = userProvider.accessToken;
-      final isOnLogin = state.fullPath == '/';
+      final hasToken = accessToken != null && accessToken.isNotEmpty;
+      final hasDetail = userProvider.hasDetailInfo;
 
-      if (!isInitialized) {
-        return null;
+      final currentLoc = state.fullPath!;
+
+      if (!isInitialized) return null;
+
+      // 보호된 페이지 모두 나열 - 추가 시 반영 필요.
+      final isProtected = [
+        '/index',
+        '/home',
+        '/chat_room',
+        '/notice_list',
+        '/notice_view',
+        '/chat_message',
+        '/preference',
+      ].any((path) => currentLoc.startsWith(path));
+
+      if (!hasToken && isProtected) {
+        return '/';
       }
 
-      final hasToken = accessToken != null && accessToken.isNotEmpty;
+      if (hasToken && !hasDetail && currentLoc != '/register-detail') {
+        return '/register-detail';
+      }
 
-      if (isOnLogin && hasToken) {
+      if (hasToken && hasDetail && currentLoc == '/') {
         return '/index';
       }
 
       return null;
     },
     routes: [
-      GoRoute(
-        path: '/preference',
-        builder: (context, state) => const Preference(),
-      ),
-      GoRoute(path: '/', builder: (context, state) => const Login()),
+      GoRoute(path: '/', builder: (context, state) => const RegisterDetail()),
       GoRoute(path: '/index', builder: (context, state) => const Index()),
       GoRoute(path: '/home', builder: (context, state) => const Home()),
       GoRoute(
@@ -68,6 +83,14 @@ GoRouter createRouter(UserProvider userProvider) {
           final noticeSeq = int.parse(state.pathParameters['noticeSeq']!);
           return NoticeView(noticeSeq: noticeSeq);
         },
+      ),
+      GoRoute(
+        path: '/preference',
+        builder: (context, state) => const Preference(),
+      ),
+      GoRoute(
+        path: '/register-detail',
+        builder: (context, state) => const RegisterDetail(),
       ),
     ],
   );
