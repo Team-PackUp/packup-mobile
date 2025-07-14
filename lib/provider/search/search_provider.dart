1252193f084fa6e102081891b672ae4d42707b8c
imjuna <dlawnsdk04@naver.com>
2025-07-14 17:11:47 +0900

    feat: search 키보드 닫기 버튼 추가

--- lib/provider/search/search_provider.dart
+++ lib/provider/search/search_provider.dart
@@ -2,14 +2,16 @@ import 'package:flutter/foundation.dart';
 
 class SearchProvider with ChangeNotifier {
 
+  late String _searchText;
+
   
   // 대상 상품 리스트 init 조회
   initContent() {
     print("검색 대상 리스트 조회를 시작합니다.");
   }
-  
 
-  Future<void> searchContent(String searchText) async {
+  Future<void> filterContent(String searchText) async {
     print('검색어: $searchText');
+    _searchText = searchText;
   }
 }
--- lib/view/search/search.dart
+++ lib/view/search/search.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:packup/Const/color.dart';
 import 'package:packup/provider/search/search_provider.dart';
 import 'package:packup/widget/common/custom_empty_list.dart';
 import 'package:provider/provider.dart';
@@ -27,11 +26,21 @@ class SearchContent extends StatefulWidget {
 class _SearchContentState extends State<SearchContent> {
   late final SearchProvider provider;
   late TextEditingController _controller;
+  late FocusNode _focusNode;
+
+  bool _showKeyboardCloseButton = false;
 
   @override
   void initState() {
     super.initState();
     _controller = TextEditingController();
+    _focusNode = FocusNode();
+
+    _focusNode.addListener(() {
+      setState(() {
+        _showKeyboardCloseButton = _focusNode.hasFocus;
+      });
+    });
 
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       provider = context.read<SearchProvider>()..initContent();
@@ -41,62 +50,102 @@ class _SearchContentState extends State<SearchContent> {
   @override
   void dispose() {
     _controller.dispose();
+    _focusNode.dispose();
     super.dispose();
   }
 
   void _onSearchChanged(String searchText) {
-
-    provider.searchContent(searchText);
+    provider.filterContent(searchText);
   }
 
   @override
   Widget build(BuildContext context) {
+    final viewInsets = MediaQuery.of(context).viewInsets;
+
     return Scaffold(
-      body: Column(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
         children: [
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () => Navigator.pop(context),
+          Column(
+            children: [
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    Expanded(
-                      child: TextField(
-                        autofocus: true,
-                        controller: _controller,
-                        decoration: const InputDecoration(
-                          hintText: '검색어를 입력해주세요',
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.arrow_back),
+                          onPressed: () => Navigator.pop(context),
                         ),
-                        onChanged: _onSearchChanged,
+                        Expanded(
+                          child: TextField(
+                            autofocus: true,
+                            focusNode: _focusNode,
+                            controller: _controller,
+                            decoration: const InputDecoration(
+                              hintText: '검색어를 입력해주세요',
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                            ),
+                            onChanged: _onSearchChanged,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+              Padding(
+                padding: EdgeInsets.only(bottom: viewInsets.bottom),
+                child: CustomEmptyList(
+                  message: '검색 결과가 존재 하지 않습니다요',
+                  icon: Icons.flight,
+                ),
+              ),
+            ],
+          ),
+
+
+          if (_showKeyboardCloseButton)
+            Positioned(
+              bottom: viewInsets.bottom,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.grey[100],
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        FocusScope.of(context).unfocus(); // 키보드 내림
+                        setState(() {
+                          _showKeyboardCloseButton = false; // 버튼 즉시 숨김
+                        });
+                      },
+                      child: const Text(
+                        '닫기',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black54,
+                        ),
                       ),
                     ),
-                    Icon(Icons.search, color: PRIMARY_COLOR),
                   ],
                 ),
               ),
             ),
-          ),
-          Expanded(
-            child: CustomEmptyList(
-              message: '검색 결과가 존재 하지 않습니다요',
-              icon: Icons.flight,
-            ),
-          ),
         ],
       ),
     );
   }
-
 }
