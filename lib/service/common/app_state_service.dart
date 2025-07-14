cd72e4de891992cc2c62f53d10c3f84bda0d58f7
jmj <waysoleward01@naver.com>
2025-07-14 16:12:27 +0900

    feat: accessToken 만료 전 socket init 수행

--- lib/service/common/app_state_service.dart
+++ lib/service/common/app_state_service.dart
@@ -1,12 +1,19 @@
+import 'dart:async';
+
+import 'package:get/get.dart';
+import 'package:dio/dio.dart';
+import 'package:go_router/go_router.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:packup/Common/util.dart';
+import 'package:packup/const/const.dart';
 import 'package:packup/service/common/socket_service.dart';
 
 class AppStateService with WidgetsBindingObserver {
-
   static final AppStateService _instance = AppStateService._internal();
 
   factory AppStateService() => _instance;
+  Timer? _refreshTokenTimer;
 
   AppStateService._internal() {
     WidgetsBinding.instance.addObserver(this);
@@ -34,6 +41,7 @@ class AppStateService with WidgetsBindingObserver {
   // 앱 종료 등 필요 시 호출 (보통 수동 호출)
   void dispose() {
     WidgetsBinding.instance.removeObserver(this);
+    stopTokenRefreshTimer();
   }
 
   // 상태별 앱 처리
@@ -42,9 +50,11 @@ class AppStateService with WidgetsBindingObserver {
     switch (_state) {
       case AppLifecycleState.paused:
         SocketService().disconnect();
+        stopTokenRefreshTimer();
         break;
 
       case AppLifecycleState.resumed:
+        startTokenRefreshTimer();
         SocketService().initConnect();
         break;
 
@@ -53,5 +63,37 @@ class AppStateService with WidgetsBindingObserver {
       case AppLifecycleState.hidden:
         break;
     }
+  }
+
+  void startTokenRefreshTimer() {
+    _refreshTokenTimer?.cancel();
+
+    _refreshTokenTimer = Timer.periodic(Duration(minutes: 25), (timer) async {
+      final refreshToken = await getToken(REFRESH_TOKEN);
+      try {
+        final dio = Dio();
+        final res = await dio.post(
+          '${dotenv.env['HTTP_URL']!}/auth/refresh',
+          data: {'refreshToken': refreshToken},
+        );
+        final newAccessToken = res.data['response']['accessToken'];
+        await saveToken(ACCESS_TOKEN, newAccessToken);
+
+        print("accessToken 타이머 수행 → 소켓 재연결");
+        SocketService().disconnect();
+        await SocketService().initConnect();
+      } catch (e) {
+        print("accessToken 타이머 실패 → 로그아웃");
+        await deleteToken(ACCESS_TOKEN);
+        await deleteToken(REFRESH_TOKEN);
+        Get.key.currentContext?.go('/login');
+      }
+    });
+  }
+
+  void stopTokenRefreshTimer() {
+    _refreshTokenTimer?.cancel();
+    _refreshTokenTimer = null;
+    print("[AppStateService] accessToken 자동 갱신 타이머 중지");
   }
 }
