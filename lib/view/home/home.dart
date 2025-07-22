bd7b28603dee445a41e5ef86eba3d5ff499aa053
jmj <waysoleward01@naver.com>
2025-07-22 18:47:00 +0900

    feat: 네비게이션 바에 맞춰서 이름 변경 tour -> home

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,94 +1,117 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:packup/provider/search/search_provider.dart';
+import 'package:packup/provider/alert_center/alert_center_provider.dart';
+import 'package:packup/provider/user/user_provider.dart';
+import 'package:packup/widget/banner/banner_section.dart';
+import 'package:packup/widget/common/alert_bell.dart';
+import 'package:packup/widget/common/custom_appbar.dart';
+import 'package:packup/widget/common/custom_sliver_appbar.dart';
+import 'package:packup/widget/guide/guide_section.dart';
+import 'package:packup/widget/profile/reward/reward_section.dart';
+import 'package:packup/widget/search/category_section.dart';
+import 'package:packup/widget/search/search.dart';
+import 'package:packup/widget/tour/hot_tour_section.dart';
 import 'package:provider/provider.dart';
-
-import 'package:packup/const/color.dart';
-
-import 'package:packup/view/payment/toss/toss_home_screen.dart';
-import 'package:packup/view/payment/toss/toss_intro_screen.dart';
-import 'package:packup/view/payment/toss/toss_payment_screen.dart';
-import 'package:packup/view/payment/toss/toss_result_screen.dart';
-import 'package:packup/view/payment/toss/toss_widget_home_screen.dart';
-import 'package:packup/view/payment/toss/toss_widget_payment_screen.dart';
-
-import '../../widget/search/search.dart';
+import 'package:packup/provider/tour/tour_provider.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => TourProvider(),
+      child: HomeContent(),
+    );
+  }
+}
+
+class HomeContent extends StatefulWidget {
+  const HomeContent({super.key});
+
+  @override
+  State<HomeContent> createState() => _HomeContentState();
+}
+
+class _HomeContentState extends State<HomeContent> {
+  late final TourProvider provider;
+  late final AlertCenterProvider _alertCenterProvider;
+
+  // 무한스크롤
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      provider = context.read<TourProvider>();
+      _alertCenterProvider = context.read<AlertCenterProvider>();
+      context.read<TourProvider>().getTourList();
+    });
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+          _scrollController.position.maxScrollExtent - 100) {
+        if (provider.hasNextPage && !provider.isLoading) {
+          provider.getTourList();
+        }
+      }
+    });
+  }
+
+  // 메모리 leak
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final profileUrl =
+        context.watch<UserProvider>().userModel?.profileImagePath;
+    final screenH = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: PRIMARY_COLOR,
-        title: Text('Home', style: TextStyle(color: TEXT_COLOR_W)),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(padding: const EdgeInsets.only(left: 10, right: 10)),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const TossResultScreen(),
+      body: SafeArea(
+        child: NestedScrollView(
+          headerSliverBuilder:
+              (context, _) => [
+                CustomSliverAppBar(
+                  title: 'PACKUP Explorer',
+                  arrowFlag: false,
+                  alert: AlertBell(),
+                  profile: CircleAvatar(
+                    radius: MediaQuery.of(context).size.height * 0.02,
+                    backgroundImage:
+                        (profileUrl != null && profileUrl.isNotEmpty)
+                            ? NetworkImage(profileUrl)
+                            : null,
                   ),
-                );
-              },
-              child: const Text('예시코드-TossResultScreen'),
+                  bottom: CustomSearch(
+                    onTap: () => context.push('/search/all'),
+                  ),
+                ),
+              ],
+          body: ListView(
+            padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.01,
+              vertical: MediaQuery.of(context).size.height * 0.01,
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const WidgetHome()),
-                );
-              },
-              child: const Text('예시코드-WidgetHome'),
-            ),
-            // const SizedBox(height: 20),
-            // ElevatedButton(
-            //   onPressed: () {
-            //     Navigator.push(
-            //       context,
-            //       MaterialPageRoute(
-            //         builder: (context) => const PaymentWidgetExamplePage(),
-            //       ),
-            //     );
-            //   },
-            //   child: const Text('예시코드'),
-            // ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const WidgetHome()),
-                );
-              },
-              child: const Text('예시코드-WidgetHome'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                context.push('/reply_list/4/REPLY_TOUR');
-              },
-              child: const Text('댓글 리스트'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                context.push('/reply_write/4/REPLY_TOUR');
-              },
-              child: const Text('댓글 신규 작성'),
-            ),
-          ],
+            children: [
+              SizedBox(height: screenH * 0.03),
+              const BannerSection(),
+              SizedBox(height: screenH * 0.03),
+              const CategorySection(),
+              SizedBox(height: screenH * 0.03),
+              const HotTourSection(),
+              SizedBox(height: screenH * 0.03),
+              const GuideSection(),
+              SizedBox(height: screenH * 0.03),
+              const RewardSection(),
+              SizedBox(height: screenH * 0.03),
+            ],
+          ),
         ),
       ),
     );
