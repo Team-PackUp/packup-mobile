46b801674919d75a7a33a174165c931593cea8b6
jmj <waysoleward01@naver.com>
2025-07-25 19:54:23 +0900

    refactor: 임포트 정리

--- lib/widget/guide/detail/section/review_list_section.dart
+++ lib/widget/guide/detail/section/review_list_section.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:packup/model/guide/guide_review_model_temp.dart';
 import 'package:packup/widget/guide/detail/review_list.dart';
-import 'package:packup/widget/guide/detail/section.dart';
 
 class ReviewListSection extends StatelessWidget {
   const ReviewListSection({super.key});
