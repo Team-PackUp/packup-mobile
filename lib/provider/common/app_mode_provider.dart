9c7f47dc7a809badfc139a667859a810e280acc1
jmj <waysoleward01@naver.com>
2025-08-18 18:21:36 +0900

    feat: 가이드 모드 전환 프로바이더 구현

---
+++ lib/provider/common/app_mode_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:packup/model/common/app_mode.dart';
+
+class AppModeProvider extends ChangeNotifier {
+  static const _k = 'app_mode';
+  AppMode _mode = AppMode.user;
+  bool _bootstrapped = false;
+
+  AppMode get mode => _mode;
+  bool get ready => _bootstrapped;
+
+  Future<void> load() async {
+    final sp = await SharedPreferences.getInstance();
+    final s = sp.getString(_k);
+    _mode = s == 'guide' ? AppMode.guide : AppMode.user;
+    _bootstrapped = true;
+    notifyListeners();
+  }
+
+  Future<void> setMode(AppMode m) async {
+    if (_mode == m) return;
+    _mode = m;
+    final sp = await SharedPreferences.getInstance();
+    await sp.setString(_k, m == AppMode.guide ? 'guide' : 'user');
+    notifyListeners();
+  }
+
+  Future<void> toggle() async =>
+      setMode(_mode == AppMode.user ? AppMode.guide : AppMode.user);
+}
