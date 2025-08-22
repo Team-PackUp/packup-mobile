4dd507089f40a410812990dee04021f9542e2e85
jmj <waysoleward01@naver.com>
2025-08-22 17:55:50 +0900

    feat: 리스팅 관련 api 연동

--- lib/service/tour/tour_service.dart
+++ lib/service/tour/tour_service.dart
@@ -2,16 +2,11 @@ import 'package:packup/http/dio_service.dart';
 import 'package:packup/model/common/result_model.dart';
 
 class TourService {
-
-  // Future<ResultModel> getTourList() async {
-  //   return await DioService().getRequest('/tour');
-  // }
-
-  Future<ResultModel> getTourList({required int page, required int size}) async {
-    final query = {
-      'page': page,
-      'size': size,
-    };
+  Future<ResultModel> getTourList({
+    required int page,
+    required int size,
+  }) async {
+    final query = {'page': page, 'size': size};
 
     return await DioService().getRequest('/tour/user', query);
   }
@@ -24,5 +19,10 @@ class TourService {
   Future<void> createTour(Map<String, dynamic> body) async {
     final url = '/tour/guide';
     await DioService().postRequest(url, body);
+  }
+
+  Future<ResultModel> getMyListings(int page) async {
+    final data = {'page': page};
+    return await DioService().getRequest('/tour/me/listings', data);
   }
 }
