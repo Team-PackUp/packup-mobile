52b4359647aa301e8f2fc1576b41bffa803f03d2
imjuna <dlawnsdk04@naver.com>
2025-09-10 13:48:07 +0900

    refactor: 누락 예약버튼 추가

--- lib/widget/tour/user/section/tour_exclude_section.dart
+++ lib/widget/tour/user/section/tour_exclude_section.dart
@@ -10,21 +10,28 @@ class TourExcludeSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenH = MediaQuery.of(context).size.height;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          '제외 사항',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: screenH * 0.02),
-        ...tour.excludeItems.map(
-          (item) => Padding(
-            padding: EdgeInsets.only(bottom: screenH * 0.01),
-            child: ExcludeItem(text: item),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            '제외 사항',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-        ),
-      ],
+          SizedBox(height: screenH * 0.02),
+          if(tour.excludeItems.isEmpty)
+            Text('등록된 제외 사항이 없습니다.',
+                style: TextStyle(color: Colors.grey.shade600))
+          else
+          ...tour.excludeItems.map(
+            (item) => Padding(
+              padding: EdgeInsets.only(bottom: screenH * 0.01),
+              child: ExcludeItem(text: item),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widget/tour/user/section/tour_include_section.dart
+++ lib/widget/tour/user/section/tour_include_section.dart
@@ -11,21 +11,29 @@ class TourIncludeSection extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenH = MediaQuery.of(context).size.height;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          '포함 사항',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: screenH * 0.02),
-        ...tour.includeItems.map(
-          (item) => Padding(
-            padding: EdgeInsets.only(bottom: screenH * 0.01),
-            child: IncludeItem(text: item),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            '포함 사항',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
-        ),
-      ],
+          SizedBox(height: screenH * 0.02),
+
+          if (tour.includeItems.isEmpty)
+            Text('등록된 포함 사항이 없습니다.',
+                style: TextStyle(color: Colors.grey.shade600))
+          else
+            ...tour.includeItems.map(
+                  (item) => Padding(
+                padding: EdgeInsets.only(bottom: screenH * 0.01),
+                child: IncludeItem(text: item),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
