8bb3f390b1b935cdd62ad29faf415c90d17038a1
jmj <waysoleward01@naver.com>
2025-09-10 12:37:30 +0900

    feat: PageModel 다음페이지 가져오는 로직 나노수정

--- lib/model/common/page_model.dart
+++ lib/model/common/page_model.dart
@@ -14,19 +14,23 @@ class PageModel<T> {
   });
 
   factory PageModel.fromJson(
-      Map<String, dynamic> json,
-      T Function(Map<String, dynamic>) convert,
-      ) {
-    final list = (json['objectList'] as List? ?? [])
-        .map((e) => convert(e as Map<String, dynamic>))
-        .toList();
+    Map<String, dynamic> json,
+    T Function(Map<String, dynamic>) convert,
+  ) {
+    final list =
+        (json['objectList'] as List? ?? [])
+            .map((e) => convert(e as Map<String, dynamic>))
+            .toList();
 
     return PageModel<T>(
       objectList: list,
       totalPage: json['totalPage'] ?? 0,
       totalElement: json['totalElement'] ?? 0,
       curPage: json['curPage'] ?? 0,
-      nextPageFlag: json['nextPageFlag'] ?? 0,
+      nextPageFlag:
+          json['nextPageFlag'] is bool
+              ? json['nextPageFlag'] as bool
+              : (json['nextPageFlag'] == 1 || json['nextPageFlag'] == '1'),
     );
   }
 }
