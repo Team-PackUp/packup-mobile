aec9dc1429ec1ed61ba1aadcc0d5030e2725c957
imjuna <dlawnsdk04@naver.com>
2025-09-11 13:50:11 +0900

    feat: 후기 프로필 이미지 공통 함수로 변경

--- lib/widget/reply/reply_read_card.dart
+++ lib/widget/reply/reply_read_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:packup/widget/common/circle_profile_image.dart';
 import 'package:packup/widget/reply/reply_image_gallery.dart';
 import '../common/image_viewer/image_viewer.dart';
 
@@ -37,17 +38,12 @@ class _ReplyReadCardState extends State<ReplyReadCard> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // 간단한 헤더
             Row(
               children: [
-                CircleAvatar(
-                  radius: 16,
-                  backgroundImage: widget.avatarUrl != null && widget.avatarUrl!.startsWith('http')
-                      ? NetworkImage(widget.avatarUrl!)
-                      : null,
-                  child: (widget.avatarUrl == null || widget.avatarUrl!.isEmpty)
-                      ? const Icon(Icons.person, size: 18)
-                      : null,
+                CircleProfileImage(
+                    radius: 16,
+                    imagePath: widget.avatarUrl,
+                    profileAvatar: false,
                 ),
                 const SizedBox(width: 8),
                 Text(widget.nickName, style: const TextStyle(fontWeight: FontWeight.w600)),
