34f9e9d75cebe522b265b98001bff2ee897f8fcb
imjuna <dlawnsdk04@naver.com>
2025-09-14 15:08:50 +0900

    feat: faq 로딩 상태 추가

--- lib/provider/profile/contact_center/faq_provider.dart
+++ lib/provider/profile/contact_center/faq_provider.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
 import '../../../model/profile/contact_center/faq_category_model.dart';
 import '../../../model/profile/contact_center/faq_model.dart';
 import '../../../service/common/loading_service.dart';
 import '../../../service/profile/contact_center/faq_service.dart';
 import '../../common/loading_provider.dart';
 
-class FaqProvider extends LoadingProvider {
+class FaqProvider extends ChangeNotifier {
   final _service = FaqService();
 
   /// 전체 목록
@@ -19,7 +21,11 @@ class FaqProvider extends LoadingProvider {
   List<FaqModel> get faqList        => _faqs;
   List<FaqCategoryModel> get category => _category;
 
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
   Future<void> getFaqCategory() async {
+    _isLoading = true;
     await LoadingService.run(() async {
       final res = await _service.getFaqCategory();
       final list = (res.response as List)
@@ -30,11 +36,14 @@ class FaqProvider extends LoadingProvider {
         ..clear()
         ..addAll(list);
 
+      _isLoading = false;
       notifyListeners();
     });
   }
 
   Future<void> getFaqList() async {
+    _isLoading = true;
+
     await LoadingService.run(() async {
       final res = await _service.getFaqList();
       final list = (res.response as List)
@@ -46,11 +55,15 @@ class FaqProvider extends LoadingProvider {
         ..addAll(list);
 
       _faqs = List<FaqModel>.from(_allFaqs);
+
+      _isLoading = false;
       notifyListeners();
     });
   }
 
   void filterByCategory(FaqCategoryModel category) {
+    _isLoading = true;
+
     if (category.codeName == '전체') {
       _faqs = List<FaqModel>.from(_allFaqs);
     } else {
@@ -59,6 +72,7 @@ class FaqProvider extends LoadingProvider {
           .toList();
     }
 
+    _isLoading = false;
     notifyListeners();
   }
 }
